bde889ee8797107d76292f2c04249968510e6067
Lukas_Low <code@lowsky.eu>
2023-04-21 00:19:39 +0200

    latest

--- src/components/canvas/Galaxy.jsx
+++ src/components/canvas/Galaxy.jsx
@@ -1,16 +1,15 @@
-import React, { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
-import * as THREE from "three";
+import React from "react";
+import { BufferGeometry, BufferAttribute, Color, PointsMaterial, Points, Mesh, SphereGeometry, MeshBasicMaterial } from "three";
 
 const Galaxy = () => {
   const starCount = 10000;
-  const starGeometry = new THREE.BufferGeometry();
+  const starGeometry = new BufferGeometry();
   const starPositions = new Float32Array(starCount * 3);
   const starColors = new Float32Array(starCount * 3);
 
   for (let i = 0; i < starCount; i++) {
     const i3 = i * 3;
-    const radius = Math.random() * 1000;
+    const radius = 700 + Math.random() * 300;
     const spinAngle = radius * Math.PI;
     const height = (Math.random() - 0.5) * 200;
 
@@ -20,31 +19,30 @@ const Galaxy = () => {
     starPositions[i3 + 1] = height;
     starPositions[i3 + 2] = Math.sin(spinAngle) * radius + Math.random() * 50 * spiralArmFactor;
 
-    const color = new THREE.Color();
-    const temperature = 4000 + Math.random() * 10000;
-    color.set(temperature, true);
+    const color = new Color();
+    color.setHSL(Math.random(), 0.7, 0.7);
     color.toArray(starColors, i3);
   }
 
-  starGeometry.setAttribute("position", new THREE.BufferAttribute(starPositions, 3));
-  starGeometry.setAttribute("color", new THREE.BufferAttribute(starColors, 3));
+  starGeometry.setAttribute("position", new BufferAttribute(starPositions, 3));
+  starGeometry.setAttribute("color", new BufferAttribute(starColors, 3));
 
-  const starMaterial = new THREE.PointsMaterial({
-    size: 0.7,
+  const starMaterial = new PointsMaterial({
+    size: 5,
     sizeAttenuation: true,
     vertexColors: true,
   });
 
-  const galaxy = new THREE.Points(starGeometry, starMaterial);
-  const galaxyRef = useRef(galaxy);
+  const galaxy = new Points(starGeometry, starMaterial);
 
-  useFrame(() => {
-    if (galaxyRef.current) {
-      galaxyRef.current.rotation.y += 0.001;
-    }
-  });
+  // Add a core
+  const core = new Mesh(
+    new SphereGeometry(30, 32, 32),
+    new MeshBasicMaterial({ color: 0xffffff })
+  );
+  galaxy.add(core);
 
-  return <primitive object={galaxyRef.current} />;
+  return <primitive object={galaxy} />;
 };
 
 export default Galaxy;
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,6 +2,6 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
-import Galaxy from "./Galaxy";
+import Galaxy from "./GalaxyCanvas";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, Galaxy, };
