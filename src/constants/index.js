068445ed076c29546a770b86e5f2e118f44fd079
Lukas_Low <code@lowsky.eu>
2023-04-21 10:32:19 +0200

    save but text too loong

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,9 @@
 import {
+  flask,
+  github_logo,
+  sql,
+  java,
+  airline,
   Grundschule,
   Hauptschule,
   HZS,
@@ -116,13 +121,29 @@ const technologies = [
     icon: git,
   },
   {
+    name: "github",
+    icon: github_logo,
+  },
+  {
     name: "docker",
     icon: docker,
   },
   {
+    name: "flask",
+    icon: flask,
+  },
+  {
     name: "python",
     icon: python,
-  }
+  },
+  {
+    name: "java",
+    icon: java,
+  },
+  {
+    name: "sql",
+    icon: sql,
+  },
 ];
 
 const education = [
@@ -280,66 +301,28 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Airline Seatbooking",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "How I learned Python with a group project at university Python is one of the most popular and versatile programming languages in the world. Whether it's for web development, data analysis, artificial intelligence, or game programming, Python has something for everyone. But what is the best way to learn Python? In this blog post, I want to tell you about my experience learning Python with a group project at university. The project: A web application using Flask and SQLAlchemy. The project was part of a web development course I took last semester. We were asked to create a web application that performed a specific function using the basics of HTML, CSS, JavaScript, and Python. We were allowed to pick our own topic, as long as it wasn't too complex or trivial. My group decided on a web application that would allow users to search, create, and rate recipes. We wanted to use it to create a platform where amateur cooks could share their passion and discover new dishes. To make this happen, we needed a backend Python framework and a database to store recipes and user data. We decided to use Flask as the backend framework and SQLAlchemy as the database abstraction layer. Flask is a minimalist framework that allows you to quickly and easily build web applications using Python. SQLAlchemy is a library that allows communicating with different databases through a unified interface. Challenges: Learning by doing The project was a big challenge for me because I had never worked with Python before. So not only did I have to learn the syntax and concepts of Python, but I also had to understand how Flask and SQLAlchemy work and how they interact with each other. Fortunately, there were many online resources to help me with this. I mainly used the official documentation of Python, Flask, and SQLAlchemy, as well as some tutorials and blogs from other developers. I learned a lot by reading code examples and experimenting with the code in my own editor. I quickly realized that Python is a very intuitive and elegant language, that offers a lot of flexibility. I especially liked the simple syntax, the use of indentation to structure the code, and the many useful functions and modules, that are included in the standard library. I also found out that Flask and SQLAlchemy work very well with Python and offer many possibilities to customize and extend the web development project and extend it. Of course there were also some difficulties and errors, that I had to fix. For example, in the beginning I had problems with understanding the difference between GET and and POST requests, routing URLs and dealing with forms and cookies. I also had to learn how to create and query database tables and query them, how to define relationships between tables and how to manage transactions. Sometimes I spent hours looking for a solution to a problem that later turned out to be a simple typo. But despite all the obstacles, the project was a lot of fun for me and gave me a sense of accomplishment. I could see how my web application was taking shape step by step and how it got more and more functions.",
     tags: [
       {
-        name: "react",
+        name: "python",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "flask",
+        color: "red-text-gradient",
+      },
+      {
+        name: "sql-lite",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "html",
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
+    image: airline,
     source_code_link: "https://github.com/",
   },
 ];
