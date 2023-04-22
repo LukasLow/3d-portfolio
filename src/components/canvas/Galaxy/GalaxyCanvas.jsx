675d936192e6c424bd16a62aa476e7e54d0a1e65
Lukas_Low <code@lowsky.eu>
2023-04-22 14:10:27 +0200

    sacew

---
+++ src/components/canvas/Galaxy/GalaxyCanvas.jsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Galaxy from "./Galaxy";
+
+const GalaxyCanvas = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 1500], fov: 40 }}>
+      <ambientLight intensity={0.7} />
+      <pointLight position={[1000, 1000, 1000]} intensity={2} />
+      <Suspense fallback={null}>
+        <OrbitControls enableZoom={false} />
+        <Galaxy />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default GalaxyCanvas;
