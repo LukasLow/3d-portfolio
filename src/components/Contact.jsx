f3934d40745ccea7ca1335ba5b9cdc52c50ea4e1
LukasLow <code@email.lowsky.eu>
2023-04-23 18:16:30 +0200

    save

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,17 +4,18 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { ContactForm } from "./ContactForm";
+import { Sociallinks } from "./Sociallinks";
 
 const Contact = () => {
   return (
     <div
       className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden justify-between`}
     >
-      <div className="w-full max-w-md">
+      <div className="w-full max-w-lg">
         <ContactForm />
       </div>
       <div className="w-full max-w-md">
-        <ContactForm />
+        <Sociallinks />
       </div>
     </div>
   );
