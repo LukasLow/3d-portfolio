6006abc9e240be18c22459536f344a9afca27ea3
LukasLow <code@lowsky.eu>
2023-04-23 12:56:22 +0200

    7

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Lukas6</span>
+            Hi, I'm <span className='text-[#915EFF]'>Lukas7</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a student
