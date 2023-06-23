874e9d4e46b1e84d38edcb25d2c26b96812a4f1f
Lukas <code@lowsky.eu>
2023-06-23 21:58:04 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a student
-            in Data Science,
+            in Physics ⚛︎,
             <br /> Uni Goettingen
           </p>
         </div>
