d2d39d8e76f6d792a64dfd8afb819bfb3a3630bd
Lukas <99747166+LukasLow@users.noreply.github.com>
2024-07-24 03:03:42 +0200

    lets goo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          commons: ['lodash', 'moment'],
+          three: ['three', 'three-stdlib'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Optional: Erhöhen Sie das Limit für Chunk-Warnungen auf 1000kB
+  },
+});
